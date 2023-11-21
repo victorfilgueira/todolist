09d227a55e31f60c01904f439bc5bea4fd1fff3f
jsimonassi <jsimonassi@id.uff.br>
2023-11-21 20:12:13 -0300

    Adicionando estrutura para o teste e2e

---
+++ appium_e2e_test/test.js
@@ -0,0 +1,30 @@
+const {remote} = require('webdriverio');
+
+const capabilities = {
+  platformName: 'Android',
+  'appium:automationName': 'UiAutomator2',
+  'appium:deviceName': 'emulator-5554',
+  'appium:appPackage': 'com.android.settings',
+  'appium:appActivity': '.Settings',
+};
+
+const wdOpts = {
+  hostname: '0.0.0.0',
+  port: 4723,
+  logLevel: 'info',
+    path: '/wd/hub',
+  capabilities,
+};
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+    const batteryItem = await driver.$('//*[@text="Battery"]');
+    await batteryItem.click();
+  } finally {
+    await driver.pause(1000);
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);
